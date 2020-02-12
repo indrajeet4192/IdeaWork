da99a213d90fd5a1caeb16a077d37c785daf33b2
indrajeet4192 <indrajeet4192@gmail.com>
2020-02-12 11:23:49 +0530

    Adding TestMain class for practice

---
+++ src/com/indra/javapractice/main/TestMain.java
@@ -0,0 +1,10 @@
+package com.indra.javapractice.main;
+
+public class TestMain {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 7;
+        int c = a/b;
+        System.out.println("c = " + c);
+    }
+}
