a337424bbdac22d51322a0a5441279d62669d713
indrajeet4192 <indrajeet4192@gmail.com>
2020-03-05 10:57:34 +0530

    Adding scala practice project

--- src/com/indra/javapractice/main/TestMain.java
+++ src/com/indra/javapractice/main/TestMain.java
@@ -4,9 +4,16 @@ public class TestMain {
     public static void main(String[] args) {
         int a = 10;
         int b = 7;
-        int c = a/b;
+        int c = a / b;
         System.out.println("c = " + c);
         System.out.println("Idea Practice");
+        char[] chararray = "asdasdsads".toCharArray();
+        char newchar = 'a';
+        System.out.println("Value of a: " + new TestMain().changeValue(10));
+    }
 
+    private int changeValue(int number) {
+        number = 17;
+        return number;
     }
 }
